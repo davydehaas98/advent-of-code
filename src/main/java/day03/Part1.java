a49f01600190964ffe71dec4a9fad650c0c210bc
Davy <davy.dehaas98@gmail.com>
2021-12-04 17:54:07 +0100

    Add day 3

---
+++ src/main/java/day03/Part1.java
@@ -0,0 +1,55 @@
+package main.java.day03;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Part1 {
+    public static void main(String[] args) {
+        List<String> binaryNumbers = getBinaryNumbers();
+        StringBuilder gammaRateStringBuilder = new StringBuilder();
+        StringBuilder epsilonRateStringBuilder = new StringBuilder();
+
+        for (int i = 0; i < 12; i++) {
+            int ratio = 0;
+
+            for (String binaryNumber : binaryNumbers) {
+                if (binaryNumber.charAt(i) == '1') ratio++;
+                else ratio--;
+            }
+
+            if (ratio < 0) {
+                gammaRateStringBuilder.append("0");
+                epsilonRateStringBuilder.append("1");
+            }
+            else {
+                gammaRateStringBuilder.append("1");
+                epsilonRateStringBuilder.append("0");
+            }
+        }
+        int gammaRate = Integer.parseInt(gammaRateStringBuilder.toString(), 2);
+        int epsilonRate = Integer.parseInt(epsilonRateStringBuilder.toString(), 2);
+        int powerConsumption = gammaRate * epsilonRate;
+        System.out.println("The power consumption of the submarine is " + powerConsumption);
+    }
+
+    private static List<String> getBinaryNumbers() {
+        List<String> binaryNumbers = new ArrayList<>();
+
+        try {
+            File file = new File("src/main/resources/day3-input.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String binaryNumber = scanner.nextLine();
+                binaryNumbers.add(binaryNumber);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return binaryNumbers;
+    }
+}
---
+++ src/main/java/day03/Part2.java
@@ -0,0 +1,87 @@
+package main.java.day03;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Part2 {
+    public static void main(String[] args) {
+        List<String> binaryNumbers = getBinaryNumbers();
+        List<String> oxygenGeneratorRatings = binaryNumbers;
+        List<String> co2ScrubberRatings = binaryNumbers;
+        int bits = binaryNumbers.get(0).length();
+
+        for (int i = 0; i < bits; i++) {
+            if (oxygenGeneratorRatings.size() > 1) {
+                int ratio = 0;
+                for (String oxygenGeneratorRating : oxygenGeneratorRatings) {
+                    if (oxygenGeneratorRating.charAt(i) == '1') ratio++;
+                    else ratio--;
+                }
+
+                List<String> filteredList = new ArrayList<>();
+
+                if (ratio < 0) {
+                    for (String oxygenGeneratorRating : oxygenGeneratorRatings) {
+                        if (oxygenGeneratorRating.charAt(i) == '0')
+                            filteredList.add(oxygenGeneratorRating);
+                    }
+                } else {
+                    for (String oxygenGeneratorRating : oxygenGeneratorRatings) {
+                        if (oxygenGeneratorRating.charAt(i) == '1')
+                            filteredList.add(oxygenGeneratorRating);
+                    }
+                }
+                oxygenGeneratorRatings = filteredList;
+            }
+
+            if (co2ScrubberRatings.size() > 1) {
+
+                int ratio = 0;
+                for (String co2ScrubberRating : co2ScrubberRatings) {
+                    if (co2ScrubberRating.charAt(i) == '1') ratio++;
+                    else ratio--;
+                }
+
+                List<String> filteredList = new ArrayList<>();
+                if (ratio < 0) {
+                    for (String co2ScrubberRating : co2ScrubberRatings) {
+                        if (co2ScrubberRating.charAt(i) == '1')
+                            filteredList.add(co2ScrubberRating);
+                    }
+                } else {
+                    for (String co2ScrubberRating : co2ScrubberRatings) {
+                        if (co2ScrubberRating.charAt(i) == '0')
+                            filteredList.add(co2ScrubberRating);
+                    }
+                }
+                co2ScrubberRatings = filteredList;
+            }
+        }
+
+        int oxygenGeneratorRating = Integer.parseInt(oxygenGeneratorRatings.get(0), 2);
+        int co2ScrubberRating = Integer.parseInt(co2ScrubberRatings.get(0), 2);
+        int lifeSupportRating = oxygenGeneratorRating * co2ScrubberRating;
+        System.out.println("The life support rating of the submarine is " + lifeSupportRating);
+    }
+
+    private static List<String> getBinaryNumbers() {
+        List<String> binaryNumbers = new ArrayList<>();
+
+        try {
+            File file = new File("src/main/resources/day3-input.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String binaryNumber = scanner.nextLine();
+                binaryNumbers.add(binaryNumber);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return binaryNumbers;
+    }
+}
