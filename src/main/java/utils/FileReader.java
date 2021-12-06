369b26775e8a766b71476bf6e9100714d77d8adc
Davy <davy.dehaas98@gmail.com>
2021-12-07 00:15:27 +0100

    Add utils

---
+++ src/main/java/utils/FileReader.java
@@ -0,0 +1,15 @@
+package utils;
+
+import lombok.SneakyThrows;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+    @SneakyThrows
+    public static List<String> readFile(String pathName) {
+        return new ArrayList<>(Files.readAllLines(Path.of(String.format("src/main/resources/%s", pathName))));
+    }
+}
