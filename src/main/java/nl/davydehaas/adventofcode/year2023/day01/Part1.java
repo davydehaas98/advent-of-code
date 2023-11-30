9baf96eae777daee276a5a07fc0bec2ae88e4382
Davy de Haas <davy.dehaas98@icloud.com>
2023-11-30 19:59:46 +0100

    Add GitHub workflow

---
+++ src/main/java/nl/davydehaas/adventofcode/year2023/day01/Part1.java
@@ -0,0 +1,4 @@
+package nl.davydehaas.adventofcode.year2023.day01;
+
+public class Part1 {
+}
---
+++ src/test/java/nl/davydehaas/adventofcode/year2020/day01/Day1Test.java
@@ -0,0 +1,21 @@
+package nl.davydehaas.adventofcode.year2020.day01;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day1Test {
+    
+    @Test
+    void part1Test() {
+        Integer result = Part1.calculate();
+        Assertions.assertThat(result)
+                .isEqualTo(437_931);
+    }
+    
+    @Test
+    void part2Test() {
+        Integer result = Part2.calculate();
+        Assertions.assertThat(result)
+                .isEqualTo(157_667_328);
+    }
+}
