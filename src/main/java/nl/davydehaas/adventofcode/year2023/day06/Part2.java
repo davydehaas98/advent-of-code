795126aaeca60b21c76df44e61c3065589367512
Davy de Haas <davy.dehaas98@icloud.com>
2023-12-06 23:38:20 +0100

    Make day 6 part 2 use a binary search

--- src/main/java/nl/davydehaas/adventofcode/year2023/day06/Part2.java
+++ src/main/java/nl/davydehaas/adventofcode/year2023/day06/Part2.java
@@ -8,7 +8,7 @@ import static nl.davydehaas.adventofcode.utils.Utils.timeSolution;
 
 class Part2 extends Year2023 {
     
-    private static final List<String> INPUT = readFile("/day06.txt");
+    private static final List<String> INPUT = readFile("/day06-test.txt");
     
     public static void main(String[] args) {
         timeSolution(Part2::calculate);
@@ -18,12 +18,25 @@ class Part2 extends Year2023 {
         long time = Long.parseLong(INPUT.get(0).replaceAll("\\D+", ""));
         long distance = Long.parseLong(INPUT.get(1).replaceAll("\\D+", ""));
         
-        for (int t = 1; t < time; t++) {
-            if (t * (time - t) > distance) {
-                return time - 2L * t + 1;
-            }
-        }
+        long boundary = findBoundary(time, 0, time / 2, distance);
         
-        return 0L;
+        return time - boundary * 2 + 1;
+    }
+    
+    private static long findBoundary(long time, long left, long right, long distance) {
+        long middle = left + (right - left) / 2;
+        long currentDistance = middle * (time - middle);
+        if (currentDistance == distance) {
+            return middle + 1;
+        } else if (currentDistance < distance) {
+            left = middle + 1;
+            return findBoundary(time, left, right, distance);
+        } else {
+            right = middle - 1;
+            if (left > right) {
+                return left;
+            }
+            return findBoundary(time, left, right, distance);
+        }
     }
 }
