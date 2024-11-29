1c41b5d460d5b862c40513081ef179026b918ba1
Davy de Haas <davy.dehaas98@gmail.com>
2024-11-29 13:57:40 +0000

    Add 2015 day 1

--- src/main/java/nl/davydehaas/adventofcode/year2015/day01/Part2.java
+++ src/main/java/nl/davydehaas/adventofcode/year2015/day01/Part2.java
@@ -1,29 +1,31 @@
 package nl.davydehaas.adventofcode.year2015.day01;
 
-import java.util.List;
-
 import static nl.davydehaas.adventofcode.util.Utils.readFile;
 import static nl.davydehaas.adventofcode.util.Utils.timeSolution;
 
-class Part1 {
+import java.util.List;
+
+class Part2 {
     
     private static final List<String> INPUT = readFile(2015, 1);
     
     public static void main(String[] args) {
-        timeSolution(Part1::solve);
+        timeSolution(Part2::solve);
     }
     
     static int solve() {
         int floor = 0;
         char[] instructions = INPUT.getFirst().toCharArray();
 
-        for (char instruction : instructions) {
-            switch (instruction) {
+        for (int i = 0; i < instructions.length; i++) {
+            switch (instructions[i]) {
                 case '(' -> floor++;
                 case ')' -> floor--;
             }
+            if (floor < 0) {
+                return i + 1;
+            }
         }
-
-        return floor;
+        throw new RuntimeException();
     }
 }
