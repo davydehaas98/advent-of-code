3ec1d2e52f91671b62ec417f56a25c8e571ef14d
Davy de Haas <davy.dehaas98@icloud.com>
2024-12-01 19:56:42 +0100

    Add year 2015 day 2

---
+++ src/main/java/nl/davydehaas/adventofcode/year2015/day02/Part1.java
@@ -0,0 +1,39 @@
+package nl.davydehaas.adventofcode.year2015.day02;
+
+import static nl.davydehaas.adventofcode.util.Utils.readFile;
+import static nl.davydehaas.adventofcode.util.Utils.timeSolution;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Part1 {
+
+    private static final List<String> INPUT = readFile(2015, 2);
+
+    public static void main(String[] args) {
+        timeSolution(Part1::solve);
+    }
+
+    static long solve() {
+        return INPUT.stream()
+                .map(dimensions -> Arrays.stream(dimensions.split("x"))
+                        .map(Integer::parseInt).toArray(Integer[]::new))
+                .mapToInt(dimensions -> calculateWrapping(dimensions[0], dimensions[1], dimensions[2]))
+                .sum();
+    }
+
+    private static int calculateWrapping(int l, int w, int h) {
+        int x = l * w;
+        int y = w * h;
+        int z = h * l;
+
+        int total = 2 * x + 2 * y + 2 * z;
+
+        if (x <= y && x <= z) {
+            return total + x;
+        } else if (y <= x && y <= z) {
+            return total + y;
+        }
+        return total + z;
+    }
+}
---
+++ src/main/java/nl/davydehaas/adventofcode/year2015/day02/Part2.java
@@ -0,0 +1,62 @@
+package nl.davydehaas.adventofcode.year2015.day02;
+
+import static nl.davydehaas.adventofcode.util.Utils.readFile;
+import static nl.davydehaas.adventofcode.util.Utils.timeSolution;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Part2 {
+
+    private static final List<String> INPUT = readFile(2015, 2);
+
+    public static void main(String[] args) {
+        timeSolution(Part2::solve);
+    }
+
+    static int solve() {
+        return INPUT.stream()
+                .map(dimensions -> Arrays.stream(dimensions.split("x"))
+                        .map(Integer::parseInt).toArray(Integer[]::new))
+                .mapToInt(dimensions -> calculateRibbon(dimensions[0], dimensions[1], dimensions[2]))
+                .sum();
+    }
+
+    private static int calculateRibbon(int l, int w, int h) {
+        int lowestX;
+        if (l < w) {
+            lowestX = Math.min(l, h);
+        } else {
+            lowestX = Math.min(w, h);
+        }
+
+        int lowestY;
+        if (l >= lowestX) {
+            if (l < w) {
+                lowestY = Math.min(l, h);
+            } else {
+                lowestY = Math.min(l, w);
+            }
+        }
+        if (w >= lowestX) {
+            if (w < l) {
+                lowestY = Math.min(w, h);
+            } else {
+                lowestY = Math.min(w, h);
+            }
+        }
+        if (h >= lowestX) {
+            if (h < l) {
+                lowestY = Math.min(h, w);
+            } else {
+                lowestY = Math.min(h, l);
+            }
+        }
+        else {
+            lowestY = lowestX;
+        }
+
+        int bow = l * w * h;
+        return lowestX * 2 + lowestY * 2 + bow;
+    }
+}
